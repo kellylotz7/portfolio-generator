d02f162e74faa09f731e340bebb57c7420f5629f
Kelly Lotz <kellylotz7@gmail.com>
2021-05-30 17:25:17 -0600

    used fs to create html page

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+const [name, github] = profileDataArgs;
 
-  };
+  fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
   
-  printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
